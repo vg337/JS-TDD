16b36d35f8b1740abb1c6c5586beb4cd8d9f438c
Vanouna <grava.vanouna@gmail.com>
2019-07-13 20:57:24 +0200

    Ajout travail TDD

---
+++ test/capitalizeFirstLetter.test.js
@@ -0,0 +1,22 @@
+// capitalizeFirstLetter.test.js
+const assert = require('assert');
+const capitalizeFirstLetter = require('../capitalizeFirstLetter');
+
+describe('capitalizeFirstLetter', () => {
+  it('is a function that accepts one argument', () => {
+    assert.strictEqual(typeof capitalizeFirstLetter, 'function');
+    assert.strictEqual(capitalizeFirstLetter.length, 1);
+  });
+  it('transforms javaScript correctly', () => {
+    assert.strictEqual(capitalizeFirstLetter('javaScript'), 'JavaScript');
+  });
+  it('works for a 1-character string', () => {
+      assert.strictEqual(capitalizeFirstLetter('i'), 'I');
+  });
+  it('works for a 1-character string', () => {
+    assert.strictEqual(capitalizeFirstLetter('i am learning TDD'), 'I am learning TDD');
+  });
+  it('works for an empty string', () => {
+    assert.strictEqual(capitalizeFirstLetter(''), '');
+  });
+});
