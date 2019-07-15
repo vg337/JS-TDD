e7a17e46b68a9991bcac0fa0576bab07e0b2884d
vg337 <52410625+vg337@users.noreply.github.com>
2019-07-15 14:12:03 +0200

    Update capitalizeFirstLetter.js

--- capitalizeFirstLetter.js
+++ capitalizeFirstLetter.js
@@ -1,9 +1,12 @@
 // capitalizeFirstLetter.js
 
 function capitalizeFirstLetter(input) {
-  return input.length > 0
-    ? input[0].toUpperCase() + input.slice(1)
-    : '';
+  var chaine = input.split(" ");
+  return chaine.map(function(majMot){
+    majMot[0].toUpperCase() + majMot.slice(1);
+    return majMot;
+  });
+  .join(" ");
 }
 //ne pas toucher
 module.exports = capitalizeFirstLetter;
